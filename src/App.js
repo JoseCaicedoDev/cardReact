323ac340ef7bc75d2a40320acc61a86c15409b77
jcaicedo <jose.caicedo@outlook.com>
2022-09-13 21:52:09 -0500

    fix(responsive card)

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className='container mx-auto'>
+      <div className='flex-col container mx-auto'>
         <h1 className='text-gray-700 font-extrabold text-2xl sm:text-5xl my-7'>Testimonials cards with react</h1>
         {
           testimonies != null ?
